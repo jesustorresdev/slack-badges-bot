13cc19f1a18b1e32cf90bec8c243efbef84e7d8c
Martin <alu0100832211@ull.edu.es>
2019-08-04 20:53:04 +0100

    Descarga imagenes desde urls

--- slack_badges_bot/adapters/api.py
+++ slack_badges_bot/adapters/api.py
@@ -13,6 +13,7 @@ import re
 import inspect
 
 from aiohttp import web
+from aiohttp import ClientSession
 from pathlib import Path
 from io import BytesIO
 
@@ -26,10 +27,6 @@ __contact__ = "jmtorres@ull.es"
 __license__ = "Apache License, Version 2.0"
 __copyright__ = "Copyright 2019 {0} <{1}>".format(__author__, __contact__)
 
-
-URL = 'url'
-BASE64 = 'base64'
-
 class WebService:
 
     def __init__(self, config: ConfigService, badge_service: BadgeService):
@@ -37,7 +34,6 @@ class WebService:
         self.badge_service = badge_service
         self.app = web.Application()
         self._setup_routes()
-        self.validated_encoding = None
         print(inspect.getmro(WebService))
 
     @validate(
@@ -77,12 +73,13 @@ class WebService:
             )
     async def create_badge_handler(self, request_json, request):
         try:
-            #request_json = await request.json()
-            # Validar
-            self.extra_validation(request_json)
-            ## Crear medalla
-            self.badge_service.create(name=request_json['name'], description=request_json['description'],
-                                      criteria=request_json['criteria'], image=self.validated_image_bytes)
+            # Validar si existe nombre de medalla, tipo de imagen, etc.
+            await self.extra_validation(request_json)
+            # Crear medalla
+            self.badge_service.create(name=request_json['name'],
+                                      description=request_json['description'],
+                                      criteria=request_json['criteria'],
+                                      image=self.validated_image_bytes)
             # Respuesta
             return web.Response(text=json.dumps(\
                     {'status': 'success'}),\
@@ -90,37 +87,28 @@ class WebService:
         except Exception as error:
             traceback.print_exc(file=sys.stdout)
             return web.Response(text=json.dumps(\
-                    {'status': 'bad request', 'error': f'{str(error)}'}),\
+                    {'status': 'bad request', 'error': f'{error}'}),\
                     status=400)
 
-    def extra_validation(self, request_json: dict):
+    async def extra_validation(self, request_json: dict):
         if self.badge_service.name_exists(request_json["name"]):
             raise BadgeCreateError("Badge already exists!")
-        self.validated_encoding = self.image_encoding(request_json['image'])
-        if self.validated_encoding is None:
-            raise BadgeCreateError(f'Image has not a valid encoding!')
         image = request_json['image']
-        if self.validated_encoding == BASE64:
+        if self.isbase64(image):
             self.validated_image_bytes = self.b64tobytes(image)
-        elif self.validated_encoding == URL:
-            self.validated_image_bytes = self.urltobytes(image)
+        elif self.isurl(image):
+            self.validated_image_bytes = await self.urltobytes(image)
+        else:
+            raise BadgeCreateError(f'image field has not a valid encoding!')
+
         if self.badge_service.validate_image(self.validated_image_bytes):
             return True
 
-    def image_encoding(self, image: str):
-        assert isinstance(image, str), 'Not an instance of str'
-        if self.isurl(image):
-            return URL
-        elif self.isbase64(image):
-            return BASE64
-        else:
-            return None
-
     def isbase64(self, data: str):
         try:
-            pattern = 'data:image\/[a-zA-Z]*;base64'
-            prefix, data = data.split(',')
-            if re.match(pattern, prefix):
+            pattern = 'data:image\/[a-zA-Z]*;base64[^-A-Za-z0-9+/=]|=[^=]|={3,}$'
+            if re.match(pattern, data):
+                prefix, data = data.split(',')
                 base64.b64decode(data)
                 return True
             else:
@@ -130,13 +118,19 @@ class WebService:
 
     def isurl(self, data: str):
         pattern = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-        return re.match(pattern, data.strip())
+        if re.match(pattern, data.strip()):
+            return True
+        return False
 
     def b64tobytes(self, data: str):
         return BytesIO(base64.b64decode(data.split(',')[-1]))
 
-    def urltobytes(self, data: str):
-        raise NotImplementedError
+    async def urltobytes(self, url: str):
+        async with ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    return BytesIO(await resp.read())
+        raise BadgeImageError(f'WebService.urltobytes: couldn\'t dowload badge image from {url}')
 
     def _setup_routes(self):
         self.app.router.add_post('/badges/create', self.create_badge_handler)
